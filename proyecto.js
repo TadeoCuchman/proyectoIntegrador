897fac1e710f7e32c6b709f9c13a6f3b8cabffd2
Diego Becco <diego.becco@etermax.com.uy>
2022-07-08 10:01:33 -0300

    login local storage usage

--- proyecto.js
+++ proyecto.js
@@ -1,11 +1,31 @@
-const login = document.getElementsByClassName('login');
-const modal = document.getElementsByClassName('modalcontainer');
-const botonCerrarModal = document.getElementsByClassName('cerrar');
+const login = document.getElementById('login');
+const modal = document.getElementById('modal-container');
+const botonCerrarModal = document.getElementById('close');
+const company = document.getElementById('company')
+const organization = document.getElementById('organization')
 
-login[0].onclick = (() => {
-    modal[0].style.visibility = 'visible'
+login.onclick = (() => {
+    modal.classList.toggle('display')
 })
 
-botonCerrarModal[0].onclick = (() => {
-    modal[0].style.visibility = 'hidden'
+botonCerrarModal.onclick = (() => {
+    modal.classList.toggle('display')
 })
+
+company.onclick = (()=>{
+    login.innerHTML = "Hola, empresa"
+    localStorage.setItem('login','empresa')
+    modal.classList.toggle('display')
+})
+
+organization.onclick = (()=>{
+    login.innerHTML = "Hola, organización"
+    localStorage.setItem('login','organización')
+    modal.classList.toggle('display')
+})
+
+
+window.onload = function () {
+    if(localStorage.getItem('login')!= null)
+        login.innerHTML = `Hola, ${localStorage.getItem('login')}`
+}
