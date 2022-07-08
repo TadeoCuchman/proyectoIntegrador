1a9d8f867acbb31945c8565d2daaa6607719515c
leonardo goyeche <leonardo@55places.com>
2022-07-08 00:04:25 -0300

    modal implementation

--- proyecto.js
+++ proyecto.js
@@ -1,7 +1,11 @@
 const login = document.getElementsByClassName('login');
-const modal = document.getElementsByClassName('modal');
+const modal = document.getElementsByClassName('modalcontainer');
 const botonCerrarModal = document.getElementsByClassName('cerrar');
 
-let isModalOpen = false;
+login[0].onclick = (() => {
+    modal[0].style.visibility = 'visible'
+})
 
-
+botonCerrarModal[0].onclick = (() => {
+    modal[0].style.visibility = 'hidden'
+})
