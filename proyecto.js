d439f4af97530fdcabd9a96b90ebb54bd8dc5f56
leonardo goyeche <leonardo@55places.com>
2022-07-07 22:36:19 -0300

    first commit

---
+++ proyecto.js
@@ -0,0 +1,7 @@
+const login = document.getElementsByClassName('login');
+const modal = document.getElementsByClassName('modal');
+const botonCerrarModal = document.getElementsByClassName('cerrar');
+
+let isModalOpen = false;
+
+
